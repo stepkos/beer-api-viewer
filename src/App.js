c2005ddfd5e235144fa4465dfe13b659d8f22295
stepkos <stepkos@wp.pl>
2023-04-07 01:01:11 +0200

    New structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import BeerList from './compontents/BeerList';
 
 function App() {
-  return (
-    <BeerList />
-  );
+
+    const [beers, setBeers] = useState([]);
+    const [filteredBeers, setFilteredBeers] = useState([]);
+    
+    // Fetch beers from API
+    useEffect(() => {
+        const fetchAPI = async () => {
+          const data = await fetch("https://api.punkapi.com/v2/beers");
+          const beerList = await data.json();
+          setBeers(beerList);
+        };
+        fetchAPI();
+    }, []);
+
+    // Update filteredBeers
+    useEffect(() => {
+        setFilteredBeers(beers)
+    }, [beers]);
+    
+
+
+    return (
+        <BeerList beers={filteredBeers} />
+    );
 }
 
 export default App;
--- src/compontents/BeerList.js
+++ src/compontents/BeerList.js
@@ -1,19 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Beer from "./Beer";
 
-const BeerList = () => {
-
-    const [beers, setBeers] = useState([]);
-    
-    // Fetch beers from API
-    useEffect(() => {
-        const fetchAPI = async () => {
-          const data = await fetch("https://api.punkapi.com/v2/beers");
-          const beerList = await data.json();
-          setBeers(beerList);
-        };
-        fetchAPI();
-    }, []);
+const BeerList = ({ beers }) => {
     
     return (
         <div className="beer-div">
