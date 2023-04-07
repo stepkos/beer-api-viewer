f96968d4b5f71f6e25c7573958d4aa26131dae66
stepkos <stepkos@wp.pl>
2023-04-07 21:56:04 +0200

    Almost done infinity scroll

--- src/App.js
+++ src/App.js
@@ -12,36 +12,67 @@ function App() {
     });
     const [searchInput, setSearchInput] = useState('');
     const [displayOnlyLiked, setDisplayOnlyLiked] = useState(false);
-    
-    const fetchAPI = async (url) => {
-        const data = await fetch(url);
+
+    const [pageNumber, setPageNumber] = useState(1);
+    const [isLoading, setIsLoading] = useState(false);
+    const pixelsOffsetToLoadNewData = 500;
+    const scrollBeersPortion = 20;
+    const baseUrl = 'https://api.punkapi.com/v2/beers';
+    var APIUrl = new URL(baseUrl);
+
+
+    const fetchAPI = async () => {
+        await setIsLoading(true);
+        APIUrl.searchParams.set('page', pageNumber);
+        APIUrl.searchParams.set('per_page', scrollBeersPortion);
+        
+        const data = await fetch(APIUrl);
         const beerList = await data.json();
         
         // Add is_liked to beer object
         beerList.forEach(beer => beer.is_liked = likedBeersId.includes(beer.id));
-        setBeers(beerList);
+        
+        console.log('tutaj');
+        
+        if (pageNumber == 1)
+            setBeers(beerList);
+        else
+            setBeers([...beers, ...beerList]);
+        
+        await setPageNumber(pageNumber + 1);
+        await setIsLoading(false);
     };
 
-    // Fetch beers from API
+    // Call when App starts
     useEffect(() => {
-        fetchAPI("https://api.punkapi.com/v2/beers?page=1&per_page=50");
-    }, []);
+        // fetchAPI(APIUrl);
 
-    // Update filteredBeers
+        window.addEventListener('scroll', () => {
+            const currentScrollValue = window.innerHeight + window.scrollY; 
+            const loadNewDataThreshold = document.body.offsetHeight - pixelsOffsetToLoadNewData; 
+            if (currentScrollValue > loadNewDataThreshold && !isLoading) {
+                console.log("load new data");
+                fetchAPI();
+            }
+        })
+    }, [])
+
+    // Update beersQuery
     useEffect(() => {
-        let url = new URL('https://api.punkapi.com/v2/beers');
+        // To clean parameters
+        APIUrl = new URL(baseUrl);
+        setPageNumber(1);
 
         // Display only liked
         if (displayOnlyLiked)
-            url.searchParams.set("ids", likedBeersId.join('|'));
+            APIUrl.searchParams.set("ids", likedBeersId.join('|'));
     
         // Filter by name
         if (searchInput !== '')
-            url.searchParams.set('beer_name', searchInput);
+            APIUrl.searchParams.set('beer_name', searchInput);
 
-        // uri += '&page=1&per_page=50';
-        console.log(url.href);
-        fetchAPI(url.href);
+        console.log(APIUrl);
+        fetchAPI();
     }, [searchInput, likedBeersId, displayOnlyLiked]);
     
     // Save likedBeersId to local storage
