46e867e7fa928d6f691e2a82732166a08964ce88
stepkos <stepkos@wp.pl>
2023-04-07 15:53:11 +0200

    Dump and load likedBeersId on localStorage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import FilterForm from './compontents/FilterForm';
 function App() {
 
     const [beers, setBeers] = useState([]);
-    const [likedBeersId, setLikedBeersId] = useState([7, 4, 22]);
+    const [likedBeersId, setLikedBeersId] = useState(() => {
+        const storedList = localStorage.getItem('likedBeersId');
+        return storedList ? JSON.parse(storedList) : [];
+    });
     const [filteredBeers, setFilteredBeers] = useState([]);
     const [searchInput, setSearchInput] = useState('');
     const [displayOnlyLiked, setDisplayOnlyLiked] = useState(false);
@@ -14,7 +17,7 @@ function App() {
     // Fetch beers from API
     useEffect(() => {
         const fetchAPI = async () => {
-          const data = await fetch("https://api.punkapi.com/v2/beers");
+          const data = await fetch("https://api.punkapi.com/v2/beers?page=1&per_page=50");
           const beerList = await data.json();
           setBeers(beerList);
         };
@@ -40,6 +43,11 @@ function App() {
         setFilteredBeers(beersToDisplay);
     }, [beers, searchInput, likedBeersId, displayOnlyLiked]);
     
+    // Save likedBeersId to local storage
+    useEffect(() => {
+        localStorage.setItem('likedBeersId', JSON.stringify(likedBeersId));
+    }, [likedBeersId])
+
     const toogleLike = id => {
         if (likedBeersId.includes(id))
             setLikedBeersId(likedBeersId.filter(el => el !== id));
