277cb9ba63a484f7794b43d585a4290944017c4b
stepkos <stepkos@wp.pl>
2023-04-09 00:22:35 +0200

    top logo baner

--- src/compontents/FilterForm.js
+++ src/compontents/FilterForm.js
@@ -1,16 +1,29 @@
 import React from "react";
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import { Link } from "react-router-dom";
+import logoImg from '../images/logo.jpg';
 
 const FilterForm = ({ searchInput, setSearchInput, displayOnlyLiked, setDisplayOnlyLiked }) =>  (
     <div className="filter-form-wrapper">
+
+        <Link to={'/'}>
+            <img className="tmp" src={logoImg} alt="Logo" />
+        </Link>
+
         <form>
             <input 
                 type="text" 
                 value={searchInput}
                 onChange={event => setSearchInput(event.target.value)}
-                placeholder="Type to search" 
+                placeholder="Search" 
             /> 
             <label>
-                <span style={{"paddingLeft": "10px"}}>Only liked</span>
+                <span className="displayLikedText">Only liked</span>
+
+                <span className="displayLikedIcon">
+                    { displayOnlyLiked ? <AiFillHeart /> : <AiOutlineHeart /> }
+                </span>
+
                 <input 
                     type="checkbox" 
                     value={displayOnlyLiked}
