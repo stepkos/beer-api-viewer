afa3e0d1f05709ad8f28a85e10f0fdc2fa7fa7a9
stepkos <stepkos@wp.pl>
2023-04-08 16:18:09 +0200

    Working app without infinity scroll

--- src/App.js
+++ src/App.js
@@ -12,73 +12,67 @@ function App() {
     });
     const [searchInput, setSearchInput] = useState('');
     const [displayOnlyLiked, setDisplayOnlyLiked] = useState(false);
-    const [pageNumber, setPageNumber] = useState(1);
-    const [isLoading, setIsLoading] = useState(false);
+    // const [pageNumber, setPageNumber] = useState(1);
+    // const [isLoading, setIsLoading] = useState(false);
     
-    const pixelsOffsetToLoadNewData = 500;
-    const scrollBeersPortion = 5;
+    // const pixelsOffsetToLoadNewData = 100;
+    const scrollBeersPortion = 20;
     const baseUrl = 'https://api.punkapi.com/v2/beers';
-    const [APIUrl, setAPIUrl] = useState(new URL(baseUrl));
 
-    const fetchAPI = async () => {
-        APIUrl.searchParams.set('page', pageNumber);
-        APIUrl.searchParams.set('per_page', scrollBeersPortion);
+    const fetchAPI = async (url, page=1) => {
+        url.searchParams.set('page', page);
+        url.searchParams.set('per_page', scrollBeersPortion);
         
-        console.log(APIUrl.toString());
-        const data = await fetch(APIUrl);
+        console.log(url.toString());
+        const data = await fetch(url);
         const beerList = await data.json();
         
         // Add is_liked to beer object
         beerList.forEach(beer => beer.is_liked = likedBeersId.includes(beer.id));
         
-        if (pageNumber == 1)
+        if (page === 1)
             await setBeers(beerList);
         else
             await setBeers([...beers, ...beerList]);
     };
 
     // Call when App starts
-    useEffect(() => {
-        window.addEventListener('scroll', () => {
-            const wrapper = async () => {
-                const currentScrollValue = window.innerHeight + window.scrollY; 
-                const loadNewDataThreshold = document.body.offsetHeight - pixelsOffsetToLoadNewData; 
-                if (currentScrollValue > loadNewDataThreshold && !isLoading) {
-                    console.log('add new data');
-                    await setIsLoading(prevValue => !prevValue);
-                    await setPageNumber(prevPageNumber => prevPageNumber + 1);
-                }
-            }
-            wrapper();
-        })
-    }, [])
+    // useEffect(() => {
+    //     window.addEventListener('scroll', async () => {
+    //             const currentScrollValue = window.innerHeight + window.scrollY; 
+    //             const loadNewDataThreshold = document.body.offsetHeight - pixelsOffsetToLoadNewData; 
+    //             if (currentScrollValue > loadNewDataThreshold && !isLoading) {
+    //                 console.log('add new data');
+    //                 await setIsLoading(prevValue => !prevValue);
+    //                 await setPageNumber(prevPageNumber => prevPageNumber + 1);
+    //             }
+    //     });
+    // }, [])
 
-    useEffect(() => {
-        const wrapper = async () => {
-            await fetchAPI();
-            setIsLoading(false);
-        }
-        wrapper();
-    }, [pageNumber]);
+    // useEffect(() => {
+    //     const wrapper = async () => {
+    //         await fetchAPI();
+    //         setIsLoading(false);
+    //     }
+    //     wrapper();
+    // }, [pageNumber]);
 
     // Update beersQuery
     useEffect(() => {
 
         // To clean up parameters
-        // APIUrl = new URL(baseUrl);
-        setAPIUrl(new URL(baseUrl));
+        let url = new URL(baseUrl);
         
         // Display only liked
         if (displayOnlyLiked)
-            APIUrl.searchParams.set("ids", likedBeersId.join('|'));
+            url.searchParams.set("ids", likedBeersId.join('|'));
         
         // Filter by name
         if (searchInput !== '')
-            APIUrl.searchParams.set('beer_name', searchInput.replace(' ', '_'));
-        
-        setPageNumber(1);
-        fetchAPI();
-        // zobacz czy zmiana z page number 1 na 1 wywoluje useEffect
+            url.searchParams.set('beer_name', searchInput.replace(' ', '_'));
+
+        fetchAPI(url, 1);
+
     }, [searchInput, likedBeersId, displayOnlyLiked]);
     
     // Save likedBeersId to local storage
